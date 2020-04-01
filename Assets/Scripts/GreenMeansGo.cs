df8a261433bf7df8bbfa68b9f9902882f2159380
adeick <37886715+adeick@users.noreply.github.com>
2020-04-01 01:47:26 -0500

    Touching Green Will Advance Level
    As more levels are added, game will automatically advance to next level. If all levels are completed, you will restart.

---
+++ Assets/Scripts/GreenMeansGo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GreenMeansGo : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other){
+
+        if (other.CompareTag("Player")){
+            Debug.Log(SceneManager.sceneCountInBuildSettings);
+            Debug.Log(SceneManager.GetActiveScene().buildIndex);
+            Debug.Log((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
+            SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
+        }
+    }
+}
+
